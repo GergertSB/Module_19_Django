85caef87c126193adac4a514c7b29478cf7240ce
gergertsb <gergertsb@yandex.ru>
2024-11-13 17:00:16 +0300

    Домашнее задание по теме "MVT. Вывод объектов в шаблоны".

--- Plato/task1/views.py
+++ Plato/task1/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def main_page(request):
+    return render(request, 'one_task/menu.html')
+
+def shop_page(request):
+    game = Game.objects.all()
+    context = {
+        'game': game,
+    }
+    return render(request, 'one_task/shop_page.html', context)
+
+def cart_page(request):
+    return render(request, 'one_task/cart_page.html')
+
