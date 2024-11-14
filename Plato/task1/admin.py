244d75dbe628c4b863734c2a784b3836d9b017d2
gergertsb <gergertsb@yandex.ru>
2024-11-14 11:50:50 +0300

    Домашнее задание по теме "Сайт администрирования"

--- Plato/task1/admin.py
+++ Plato/task1/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+# Админка для модели Game
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'description', 'age_limited',)
+    search_fields = ('title',)
+
+# Админка для модели Buyer
+@admin.register(Buyer)
+class Buyer(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age',)
+
