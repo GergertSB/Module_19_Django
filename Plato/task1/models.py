ffd3f8df61d9cc30794127423d7b35e1bd196820
gergertsb <gergertsb@yandex.ru>
2024-11-21 10:46:49 +0300

    Домашнее задание по теме "Пагинация"

--- Plato/task1/models.py
+++ Plato/task1/models.py
@@ -1,5 +1,3 @@
-from tkinter.font import names
-
 from django.db import models
 
 # Create your models here.
@@ -17,6 +15,7 @@ class Game(models.Model):
     size = models.DecimalField(decimal_places=2, max_digits=10)
     description = models.TextField()
     age_limited = models.BooleanField(False)
+
     bayer = models.ManyToManyField(Buyer, related_query_name='games')
 
     def __str__(self):
--- Plato/task1/views.py
+++ Plato/task1/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import *
 
@@ -6,12 +7,19 @@ def main_page(request):
     return render(request, 'one_task/menu.html')
 
 def shop_page(request):
-    game = Game.objects.all()
+    game = Game.objects.all().order_by()
+    per_page = int(request.GET.get('per_page', 1))
+    paginator = Paginator(game, per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    img_list = ['image/1.png', 'image/2.png', '3.png', '4.png', '5.png']
     context = {
         'game': game,
+        'paginator': paginator,
+        'page_obj': page_obj,
+        'img_list': img_list
     }
     return render(request, 'one_task/shop_page.html', context)
 
 def cart_page(request):
     return render(request, 'one_task/cart_page.html')
-
